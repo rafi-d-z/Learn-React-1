8de57caccd2371e2b33b822ee16eb363d1948513
cooln <rafidizia@gmail.com>
2023-09-15 20:30:14 -0400

    modified index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,78 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const htmlContainer = document.getElementById("root");
+const root = ReactDOM.createRoot(htmlContainer);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function ClassList(){
+  return(
+    <div>
+      <h1>Welcome to CTP</h1>
+      <p>List of Students</p>
+      {studentList.map((student)=> (
+        <StudentInfo {...student} key={student.sID}/>
+      ))}
+    </div>
+  )
+}
+
+const studentList = [
+  {
+    firstName: "Misty",
+    lastName: "Knight",
+    sId: "234",
+    school: "Queens College",
+    major: "Law",
+  },
+  {
+    firstName: "Jessica",
+    lastName: "Jones",
+    sId: "434",
+    school: "Brooklyn College",
+    major: "CS",
+  },
+  {
+    firstName: "Colleen",
+    lastName: "Wing",
+    sId: "233",
+    school: "Queens College",
+    major: "CS",
+  },
+  {
+    firstName: "Dare",
+    lastName: "Devil",
+    sId: "876",
+    school: "CCNY",
+    major: "Law",
+  },
+  {
+    firstName: "Luke",
+    lastName: "Cage",
+    sId: "323",
+    school: "CCNY",
+    major: "Math",
+  },
+];
+
+function StudentInfo(props){
+  return(
+    <div>
+      <div>
+        {props.lastName}, {props.firstName}
+      </div>
+      <ul>
+        <li>
+          <strong>ID:</strong> {props.sId}
+        </li>
+        <li>
+          <strong>School:</strong> {props.school}
+        </li>
+        <li>
+          <strong>Major:</strong> {props.major}
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+root.render(<ClassList />)
